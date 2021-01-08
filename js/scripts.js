0c7b6b8fb2f73882d53eac1d714d5e0254c98268
Salvatore Squatrito <ssprog@gmail.com>
2021-01-08 08:37:00 +0100

    phase 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
     * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
+    * Copyright 2013-2021 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
     */
     (function($) {
